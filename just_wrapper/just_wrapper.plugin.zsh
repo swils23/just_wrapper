e97f1196304d0e35f83bd87d1964afd4e57c8f42
Sam Wilson <samuelpwilson23@gmail.com>
2024-10-08 10:43:57 -0500

    Refactoring

--- just_wrapper/just_wrapper.plugin.zsh
+++ just_wrapper/just_wrapper.plugin.zsh
@@ -1,20 +1,49 @@
 JW_COMMANDS_DIR="$ZSH_CUSTOM/plugins/just_wrapper/commands/"
 JW_COMMANDS_FILE="$JW_COMMANDS_DIR/.commands"
-chmod +x "$JW_COMMANDS_FILE"
+chmod +x "$JW_COMMANDS_DIR"/*
 
-# Get all the functions in the commands file
-JW_COMMANDS=$(awk '/^function/ {print $2}' "$JW_COMMANDS_FILE" | sed 's/()//')
-
-# Add any script in the commands directory (excluding .commands)
+JW_COMMANDS=$(awk '/^function/ {print $2}' "$JW_COMMANDS_FILE" | sed -E 's/\(\)//' | sed 's/{//')
 JW_SCRIPTS=$(ls "$JW_COMMANDS_DIR" | grep -v '.commands' | sed 's/\.sh//')
 
-# Function to print out the cases of the 'j' function
-_print_cases() {
-    local jw_default_commands
-    jw_default_commands=$(sed -n '/^function just_wrapper() {/,/esac/p' $ZSH_CUSTOM/plugins/just_wrapper/just_wrapper | grep -E '^\s*[a-zA-Z_][a-zA-Z0-9_]*\)' | sed 's/)//' | tr '\n' ' ' | xargs)
-    echo "$jw_default_commands $JW_SCRIPTS $JW_COMMANDS"
+
+function just_wrapper() {
+    if [[ $# -lt 1 ]]; then
+        echo "Usage: just_wrapper <command> [args]"
+        return 1
+    fi
+
+    case $1 in
+        # Default commands provided by just_wrapper plugin
+        just_wrapper_edit)
+            code "$JW_COMMANDS_DIR"
+            ;;
+        just_wrapper_reload)
+            exec $SHELL
+            ;;
+        just_wrapper_update)
+            bash <(curl -s https://raw.githubusercontent.com/swils23/just_wrapper/main/install_script) $ZSH_CUSTOM
+            ;;
+        *)
+            # Command overrides
+            if echo "$JW_COMMANDS" | grep -qw "$1"; then
+                "$JW_COMMANDS_FILE" "$@"
+            # Script overrides
+            elif echo "$JW_SCRIPTS" | grep -qw "$1"; then
+                "$JW_COMMANDS_DIR/$1" "${@:2}"
+            # Fallback to just
+            else
+                just "$@"
+            fi
+            ;;
+    esac
 }
 
+# region completion
+_print_cases() {
+    local jw_default_commands
+    jw_default_commands=$(sed -n '/^function just_wrapper() {/,/esac/p' $ZSH_CUSTOM/plugins/just_wrapper/just_wrapper.plugin.zsh | grep -E '^\s*[a-zA-Z_][a-zA-Z0-9_]*\)' | sed 's/)//' | tr '\n' ' ' | xargs)
+    echo "$jw_default_commands $JW_SCRIPTS $JW_COMMANDS"
+}
 _j_completions() {
     local cur prev opts just_completions
     COMPREPLY=()
@@ -38,7 +67,5 @@ _j_completions() {
 
     return 0
 }
-
 complete -F _j_completions just_wrapper
-
-autoload -Uz just_wrapper
+# endregion completion
